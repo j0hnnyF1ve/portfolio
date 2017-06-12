f642c353119b4e4f73b51a04e0b251b605656e29
John Pangilinan <john.pangilinan1@gmail.com>
2017-06-12 10:50:16 -0700

    Added Disney link to main page, and Disney page and assets

---
+++ content/disney.php
@@ -0,0 +1,55 @@
+<div class="col1">
+   <h2>Disney / ABC OTV (Owned TV) Video Project</h2>
+   <p>
+     &lt;Project Description here&gt;
+   </p>
+
+   <div class="entry">
+      <div class="thumb">
+         <a href="Magnify360/intuitProSeries/index.html" target="_new"><img src="img/intuitProSeries.png" /></a>
+         <h3><a href="Magnify360/intuitProSeries/index.html" target="_new">&lt; Image Caption Here &gt;</a></h3>
+      </div>
+      <div class="description">
+         <p>
+          &lt;Development of Page Description here&gt;
+         </p>
+      </div>
+   </div>
+
+   <div class="entry">
+      <div class="thumb">
+         <a href="Magnify360/intuitHomestead/index.html" target="_new"><img src="img/intuitHomestead.png" /></a>
+         <h3><a href="Magnify360/intuitHomestead/index.html" target="_new">&lt; Image Caption Here &gt;</a></h3>
+      </div>
+      <div class="description">
+         <p>
+           &lt;Responsiveness, 1UP, 2UP, 3UP Description here&gt;
+         </p>
+      </div>
+   </div>
+
+   <div class="entry">
+      <div class="thumb">
+         <a href="Magnify360/intuitHomestead/index.html" target="_new"><img src="img/intuitHomestead.png" /></a>
+         <h3><a href="Magnify360/intuitHomestead/index.html" target="_new">&lt; Image Caption Here &gt;</a></h3>
+      </div>
+      <div class="description">
+         <p>
+           &lt;Analytics Description here&gt;
+         </p>
+      </div>
+   </div>
+
+   <div class="entry">
+      <div class="thumb">
+         <a href="Magnify360/intuitHomestead/index.html" target="_new"><img src="img/intuitHomestead.png" /></a>
+         <h3><a href="Magnify360/intuitHomestead/index.html" target="_new">&lt; Image Caption Here &gt;</a></h3>
+      </div>
+      <div class="description">
+         <p>
+           &lt;Prototypes/prototyping Description here&gt;
+         </p>
+      </div>
+   </div>
+
+</div>
