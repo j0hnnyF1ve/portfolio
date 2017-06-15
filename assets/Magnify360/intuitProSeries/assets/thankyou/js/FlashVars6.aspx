3a4234c6a52083ea1735112dcc222811c5812e91
John Pangilinan <john.pangilinan1@gmail.com>
2017-06-15 11:37:13 -0700

    Moved Magnify360 assets to the 'assets' folder, changed links on Magnify360 page

---
+++ assets/Magnify360/intuitProSeries/assets/thankyou/js/FlashVars6.aspx
@@ -0,0 +1,1 @@
+buttonurl1=http://proseries.intuit.com/products/professional/&btn1cta=Read+Review
--- content/magnify360.php
+++ content/magnify360.php
@@ -1,3 +1,15 @@
+<?php
+  $magUrl = [
+    'assets/Magnify360/intuitProSeries/index.html',
+    'assets/Magnify360/intuitHomestead/index.html'
+  ];
+
+  $imgUrl = [
+    'assets/Magnify360/intuitProSeries.png',
+    'assets/Magnify360/intuitHomestead.png'
+  ];
+?>
+
 <div class="col1">
    <h2>Magnify360 Projects</h2>
    <p>
@@ -7,8 +19,8 @@
 
    <div class="entry">
       <div class="thumb">
-         <a href="Magnify360/intuitProSeries/index.html" target="_new"><img src="img/intuitProSeries.png" /></a>
-         <h3><a href="Magnify360/intuitProSeries/index.html" target="_new">Intuit ProSeries Tax Variation</a></h3>
+         <a href="<?php echo $magUrl[0]; ?>" target="_new"><img src="<?php echo $imgUrl[0]; ?>" /></a>
+         <h3><a href="<?php echo $magUrl[0]; ?>" target="_new">Intuit ProSeries Tax Variation</a></h3>
       </div>
       <div class="description">
          <p>
@@ -21,8 +33,8 @@
 
    <div class="entry">
       <div class="thumb">
-         <a href="Magnify360/intuitHomestead/index.html" target="_new"><img src="img/intuitHomestead.png" /></a>
-         <h3><a href="Magnify360/intuitHomestead/index.html" target="_new">Intuit Homestead Variation</a></h3>
+         <a href="<?php echo $magUrl[1]; ?>" target="_new"><img src="<?php echo $imgUrl[1]; ?>" /></a>
+         <h3><a href="<?php echo $magUrl[1]; ?>" target="_new">Intuit Homestead Variation</a></h3>
       </div>
       <div class="description">
          <p>
@@ -31,6 +43,6 @@
             tab section displays different content for that tab.
          </p>
       </div>
-   </div>   
-  
+   </div>
+
 </div>
